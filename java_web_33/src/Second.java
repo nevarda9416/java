e6151b9a99879bd4c851be8aa18883db4878cfd5
TuDT <tudt@codluck.com>
2023-08-15 16:45:17 +0700

    - [Java Web NIIT-ICT 33]: Lession Type Casting

--- java_web_33/src/Second.java
+++ java_web_33/src/Second.java
@@ -13,5 +13,22 @@ public class Second {
         System.out.println("Age: " + myObj.age);
         System.out.println("Graduation Year: " + myObj.graduationYear);
         myObj.study();
+        /**
+         * Java Type Casting: 2 types
+         * Widening Casting (automatically) - converting a smaller type to a larger type size:
+         * byte -> short -> char -> int -> long -> float -> double
+         * Narrowing Casting (manually) - converting a larger type to a smaller size type:
+         * double -> float -> long -> int -> char -> short -> byte
+         */
+        // Widening Casting (automatically)
+        int myInt = 9;
+        double myDouble = myInt; // Automatic casting: int to double
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        // Narrowing Casting (manually)
+        double myDouble2 = 9.78f;
+        int myInt2 = (int) myDouble2; // Manual casting: double to int
+        System.out.println(myInt2);
+        System.out.println(myDouble2);
     }
 }
