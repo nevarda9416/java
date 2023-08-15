9e6c6a4c43135550393dc3a616f1c271a4395f75
TuDT <tudt@codluck.com>
2023-08-15 15:07:53 +0700

    - [Java Web NIIT-ICT 33]: Lession Modifiers Final/Static

--- java_web_33/src/Main.java
+++ java_web_33/src/Main.java
@@ -1,21 +1,22 @@
 public class Main {
+    // Final
+    final int x = 10;
+    final double PI = 3.14;
     // Static method
-    public static void myStaticMethod() {
+    static void myStaticMethod() {
         System.out.println("Static methods can be called without creating objects");
     }
     // Public method
     public void myPublicMethod() {
-        System.out.println("Public nethods must be called by creating objects");
-    }
-    public void fullThrottle() {
-        System.out.println("The car is going as fast as it can!");
-    }
-    public void speed(int maxSpeed) {
-        System.out.println("Max speed is: " + maxSpeed);
+        System.out.println("Public methods must be called by creating objects");
     }
     public static void main(String[] args) {
-        myStaticMethod(); // Call the static method
         Main myObj = new Main(); // Create an object of Main
-        myObj.myPublicMethod(); // Call the public method on the object
+        //myObj.x = 50; // cannot assign a value to final variable x
+        //myObj.PI = 25; // cannot assign a value to final variable PI
+        System.out.println(myObj.x);
+        myStaticMethod(); // Call the static method
+        //myPublicMethod(); // non-static method myPublicMethod() cannot be referenced from a static context
+        myObj.myPublicMethod(); // Call the public method
     }
 }
