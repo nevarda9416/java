5d299fd07cb4d18fd20ae07a527b1e6b80b14129
TuDT <tudt@codluck.com>
2023-08-15 16:59:38 +0700

    - [Java Web NIIT-ICT 33]: Lession For Loop/Multi Arrays

--- java_web_33/src/Second.java
+++ java_web_33/src/Second.java
@@ -30,5 +30,24 @@ public class Second {
         int myInt2 = (int) myDouble2; // Manual casting: double to int
         System.out.println(myInt2);
         System.out.println(myDouble2);
+        /**
+         * for each loop
+         * for (type variableName : arrayName) {
+         *      // code block to be executed
+         * }
+         */
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
+        // Multidimensional Arrays
+        int[][] myNumbers = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+        myNumbers[1][2] = 9; // Thay đổi giá trị hàng thứ 2 cột thứ 3 của mảng 2 chiều myNumbers
+        System.out.println(myNumbers[1][2]);
+        for (int i = 0; i < myNumbers.length; i++) {
+            for (int j = 0; j < myNumbers[i].length; j++) {
+                System.out.println(myNumbers[i][j]);
+            }
+        }
     }
 }
