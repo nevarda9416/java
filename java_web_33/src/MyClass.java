758eb672ee45d1c11e4fcea8231d0efd972fb9c2
TuDT <tudt@codluck.com>
2023-08-17 17:32:56 +0700

    - [Java Web NIIT-ICT 33]: Lession Polymorphism

--- java_web_33/src/MyClass.java
+++ java_web_33/src/MyClass.java
@@ -1,9 +1,25 @@
-import java.util.Scanner;
+class Animal {
+    public void animalSound() {
+        System.out.println("The animal makes a sound");
+    }
+}
+class Pig extends Animal {
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+}
+class Dog extends Animal {
+    public void animalSound() {
+        System.out.println("The dog says: bow wow");
+    }
+}
 public class MyClass {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter username");
-        String userName = myObj.nextLine();
-        System.out.println("Username is: " + userName);
+        Animal myAnimal = new Animal(); // Create a Animal
+        Animal myPig = new Pig(); // Create a Pig object
+        Animal myDog = new Dog(); // Create a Dog object
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
     }
 }
