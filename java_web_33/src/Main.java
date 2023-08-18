e74dda997827163aac816100264d1086d0f12653
TuDT <tudt@codluck.com>
2023-08-18 17:30:33 +0700

    - [Java Web NIIT-ICT 33]: Lession HashMap

--- java_web_33/src/Main.java
+++ java_web_33/src/Main.java
@@ -1,5 +1,5 @@
-import java.util.LinkedList;
 import java.util.Collections;
+import java.util.HashMap; // import the HashMap class
 
 /**
  * How the LinkedList works?
@@ -9,28 +9,40 @@ import java.util.Collections;
  */
 class Main {
     public static void main(String[] args) {
-        LinkedList<String> cars = new LinkedList<String>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Mazda");
-        // Access an Item
-        System.out.println(cars.get(2));
-        // Change an Item
-        cars.set(2, "Opel");
-        // Remove an Item
-        cars.remove(3);
-        // Find out how many elements an ArrayList have
-        System.out.println("Have " + cars.size() + " cars:");
-        // Loop Through an ArrayList
-        for (int i = 0; i < cars.size(); i++) {
-            System.out.println(cars.get(i));
-        }
-        Collections.sort(cars); // Sort cars
-        for (String i : cars) {
+        // Create a HashMap object called capitalCities
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+        // Add keys and values (Country, City) = Redis (cache)
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        capitalCities.remove("Germany");
+        System.out.println(capitalCities);
+        System.out.println(capitalCities.get("USA"));
+        System.out.println(capitalCities.size());
+        // Loop through a HashMap
+        // Print keys
+        for (String i : capitalCities.keySet()) {
             System.out.println(i);
         }
-        // Remove all Items
-        cars.clear();
+        // Print values
+        for (String i : capitalCities.values()) {
+            System.out.println(i);
+        }
+        // Print keys and values
+        for (String i : capitalCities.keySet()) {
+            System.out.println("key: " + i + ", value: " + capitalCities.get(i));
+        }
+        capitalCities.clear();
+        System.out.println(capitalCities);
+        // Create a HashMap object called people
+        HashMap<String, Integer> people = new HashMap<String, Integer>();
+        // Add keys and values (Name, Age)
+        people.put("John", 32);
+        people.put("Steve", 30);
+        people.put("Angie", 33);
+        for (String i : people.keySet()) {
+            System.out.println("key: " + i + ", value: " + people.get(i));
+        }
     }
 }
