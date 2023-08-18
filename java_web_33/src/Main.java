7775d6ae9784af3b5bd2b4c8f24f8f5182abc123
TuDT <tudt@codluck.com>
2023-08-18 16:59:52 +0700

    - [Java Web NIIT-ICT 33]: Lession LinkedList

--- java_web_33/src/Main.java
+++ java_web_33/src/Main.java
@@ -1,9 +1,15 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Collections;
 
+/**
+ * How the LinkedList works?
+ * The LinkedList stores its items in "containers". The list has a link to the first container and each container has a
+ * link to the next container in the list. To add an element to the list, the element is placed into a new container and
+ * that container is linked to one of the other containers in the list
+ */
 class Main {
     public static void main(String[] args) {
-        ArrayList<String> cars = new ArrayList<String>();
+        LinkedList<String> cars = new LinkedList<String>();
         cars.add("Volvo");
         cars.add("BMW");
         cars.add("Ford");
