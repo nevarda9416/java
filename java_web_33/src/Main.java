5a36aceaf821ad16833a5de0ca80c4d2811063d1
TuDT <tudt@codluck.com>
2023-08-18 16:36:35 +0700

    - [Java Web NIIT-ICT 33]: Lession ArrayList

--- java_web_33/src/Main.java
+++ java_web_33/src/Main.java
@@ -1,53 +1,30 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
-
-/**
- * Enum: is a special 'class' that represents a group of constants (unchangeable variables, like final variables)
- * To create an enum, use the enum keyword (instead of class or interface), and separate the constants with a comma
- */
-enum Level {
-    LOW,
-    MEDIUM,
-    HIGH
-}
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Main {
     public static void main(String[] args) {
-        LocalDateTime myDateObj = LocalDateTime.now();
-        System.out.println("Before formatting: " + myDateObj);
-
-        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String formattedDate = myDateObj.format(myFormatObj);
-        System.out.println("After formatting: " + formattedDate);
-
-        Scanner myVar = new Scanner(System.in);
-        System.out.println("Enter level: ");
-        Level myLevel = Level.valueOf(myVar.nextLine());
-        switch (myLevel) {
-            case LOW:
-                System.out.println("Low level");
-                break;
-            case MEDIUM:
-                System.out.println("Medium level");
-                break;
-            case HIGH:
-            default:
-                System.out.println("High level");
-                break;
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        // Access an Item
+        System.out.println(cars.get(2));
+        // Change an Item
+        cars.set(2, "Opel");
+        // Remove an Item
+        cars.remove(3);
+        // Find out how many elements an ArrayList have
+        System.out.println("Have " + cars.size() + " cars:");
+        // Loop Through an ArrayList
+        for (int i = 0; i < cars.size(); i++) {
+            System.out.println(cars.get(i));
         }
-        /**
-         * Input Types:
-         */
-        System.out.println("Enter name, age and salary: ");
-        // String input
-        String name = myVar.nextLine();
-        // Numerical input
-        int age = myVar.nextInt();
-        double salary = myVar.nextDouble();
-        // Output input by user
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Salary: " + salary);
+        Collections.sort(cars); // Sort cars
+        for (String i : cars) {
+            System.out.println(i);
+        }
+        // Remove all Items
+        cars.clear();
     }
 }
