cd9f6a8cf1f0832c4b4d54bc148074cdc23341dd
tudao <nevarda9416@gmail.com>
2023-08-21 23:17:14 +0700

    - [Java Web NIIT-ICT 33]: Lession Threads (1)

--- java_web_33/src/Main.java
+++ java_web_33/src/Main.java
@@ -1,22 +1,14 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-/**
- * We can import java.util.regex package to work with regular expressions.
- * The package includes the following classes:
- * - Pattern Class: Defines a pattern (to be used in a search)
- * - Matcher Class: Used to search for the pattern
- * - PatternSyntaxException Class: Indicates syntax error in a regular expression pattern
- */
-class Main {
+class Main extends Thread {
+    public static int amount = 0;
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("w3schools", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher("Visit W2Schools!");
-        boolean matchFound = matcher.find();
-        if (matchFound) {
-            System.out.println("Match found");
-        } else {
-            System.out.println("Match not found");
-        }
+        Main thread = new Main();
+        thread.start();
+        System.out.println(amount);
+        amount++;
+        System.out.println(amount);
+    }
+    public void run() {
+        amount++;
     }
 }
