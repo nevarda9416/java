d6f28789a312bfb06a1fbd44732bc5495e2a09bb
DAO TIEN TU <daotientu@gmail.com>
2023-08-22 20:52:25 +0700

    - [Java Web NIIT-ICT 33]: Lession 2.5

--- java_web_33/java_33/java_web/src/Main.java
+++ java_web_33/java_33/java_web/src/Main.java
@@ -14,17 +14,57 @@ public class Main {
             int pt = Integer.parseInt(scanner.nextLine());
             number_arr[i] = pt;
         }
-        int sum = 0;
-        int min = number_arr[0];
-        int max = number_arr[0];
-        for (int num : number_arr) {
-            if (num > max) max = num;
-            if (num < min) min = num;
-            sum += num;
+        boolean check = true;
+        //System.out.println(number_arr[0]);
+        while (check) {
+            System.out.println("1. Tinh tong");
+            System.out.println("2. Tinh hieu");
+            System.out.println("3. Tinh nhan");
+            System.out.println("4. Tinh chia");
+            System.out.print("Moi ban chon chuc nang: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            switch (select) {
+                case 1 -> {
+                    System.out.println("Ban da chon tinh tong");
+                    int tong = number_arr[0];
+                    for (int num : number_arr) {
+                        if (num == number_arr[0]) continue;
+                        tong += num;
+                    }
+                    System.out.println("Tong = " + tong);
+                }
+                case 2 -> {
+                    System.out.println("Ban da chon tinh hieu");
+                    int hieu = number_arr[0];
+                    for (int num : number_arr) {
+                        if (num == number_arr[0]) continue;
+                        hieu -= num;
+                    }
+                    System.out.println("Hieu = " + hieu);
+                }
+                case 3 -> {
+                    System.out.println("Ban da chon tinh nhan");
+                    int nhan = number_arr[0];
+                    for (int num : number_arr) {
+                        if (num == number_arr[0]) continue;
+                        nhan *= num;
+                    }
+                    System.out.println("Nhan = " + nhan);
+                }
+                case 4 -> {
+                    System.out.println("Ban da chon tinh chia");
+                    double chia = number_arr[0];
+                    for (int num : number_arr) {
+                        if (num == number_arr[0]) continue;
+                        chia /= num;
+                    }
+                    System.out.println("Chia = " + chia);
+                }
+                default -> {
+                    System.out.println("Vui long nhap vao tu 1 -> 4. Chuong trinh se tu dong thoat");
+                    check = false;
+                }
+            }
         }
-        System.out.println("Gia tri lon nhat la: " + max);
-        System.out.println("Gia tri nho nhat la: " + min);
-        double avg = (double) sum / number;
-        System.out.println("Gia tri trung binh la: " + avg);
     }
 }
