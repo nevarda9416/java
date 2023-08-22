20cc90d1babe148c1bd69b0b0975a9847a71211a
DAO TIEN TU <daotientu@gmail.com>
2023-08-22 20:23:15 +0700

    - [Java Web NIIT-ICT 33]: Lession 2.4

--- java_web_33/java_33/java_web/src/Main.java
+++ java_web_33/java_33/java_web/src/Main.java
@@ -5,50 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean check = true;
-        //1. them moi sinh vien
-        //2. tim kiem sinh vien
-        //3. xoa sinh vien
-        //4. thong ke
-        // Foreach: First in, first out
-        int[] number_arr = new int[6];
-        number_arr[0] = 2;
-        number_arr[1] = 3;
-        number_arr[2] = 4;
-        number_arr[3] = 5;
+        System.out.print("Nhap vao so luong cac phan tu: ");
+        int number = Integer.parseInt(scanner.nextLine());
+
+        int[] number_arr = new int[number];
         for (int i = 0; i < number_arr.length; i++) {
-            if (number_arr[i] == 4) continue;
-            System.out.println(number_arr[i]);
+            System.out.print("Nhap vao phan tu thu " + (i + 1) + ": ");
+            int pt = Integer.parseInt(scanner.nextLine());
+            number_arr[i] = pt;
         }
+        int sum = 0;
+        int min = number_arr[0];
+        int max = number_arr[0];
         for (int num : number_arr) {
-            if (num == 4) continue;
-            System.out.println(num);
+            if (num > max) max = num;
+            if (num < min) min = num;
+            sum += num;
         }
-        while (check) {
-            System.out.println("1. Them moi sinh vien");
-            System.out.println("2. Tim kiem sinh vien");
-            System.out.println("3. Xoa sinh vien");
-            System.out.println("4. Thong ke sinh vien");
-            System.out.println("Moi ban chon chuc nang");
-            int select = Integer.parseInt(scanner.nextLine());
-            switch (select) {
-                case 1:
-                    System.out.println("Ban da chon chuc nang 1");
-                    break;
-                case 2:
-                    System.out.println("Ban da chon chuc nang 2");
-                    break;
-                case 3:
-                    System.out.println("Ban da chon chuc nang 3");
-                    break;
-                case 4:
-                    System.out.println("Ban da chon chuc nang 4");
-                    break;
-                default:
-                    System.out.println("Vui long nhap vao tu 1 -> 4. Chuong trinh se tu dong thoat");
-                    check = false;
-                    break;
-            }
-        }
+        System.out.println("Gia tri lon nhat la: " + max);
+        System.out.println("Gia tri nho nhat la: " + min);
+        double avg = (double) sum / number;
+        System.out.println("Gia tri trung binh la: " + avg);
     }
 }
