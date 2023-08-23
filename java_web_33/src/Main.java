06f9426cec4588117ba3f260ccf93a31c6c09a5f
TuDT <tudt@codluck.com>
2023-08-23 18:02:20 +0700

    - [Java Web NIIT-ICT 33]: Lession Generic (multiple parameters)

--- java_web_33/src/Main.java
+++ java_web_33/src/Main.java
@@ -1,23 +1,25 @@
-class Test<T> {
-    // An object of type T is declared
-    T obj;
-
-    Test(T obj) {
-        this.obj = obj;
-    }//constructor
-
-    public T getObject() {
-        return this.obj;
+// Java program to show multiple
+// Type parameters in Java Generics
+// We use < > to specify Parameter type
+class Test<T, U> {
+    T obj1; // An object of type T
+    U obj2; // An object of type U
+    // constructor
+    Test(T obj1, U obj2) {
+        this.obj1 = obj1;
+        this.obj2 = obj2;
+    }
+    // To print objects of T and U
+    public void print() {
+        System.out.println(obj1);
+        System.out.println(obj2);
     }
 }
-
+// Driver class to test above
 class Main extends Thread {
     public static void main(String[] args) {
         // instance of Integer type
-        Test<Integer> iObj = new Test<Integer>(15);
-        System.out.println(iObj.getObject());
-        // instance of String type
-        Test<String> sObj = new Test<String>("GeeksForGeeks");
-        System.out.println(sObj.getObject());
+        Test<String, Integer> obj = new Test<String, Integer>("GfG", 15);
+        obj.print();
     }
 }
