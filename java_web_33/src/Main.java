5b722d5cdf81abfdf23c4f493445514a7620fd0e
TuDT <tudt@codluck.com>
2023-08-23 17:24:15 +0700

    - [Java Web NIIT-ICT 33]: Lession Lambda

--- java_web_33/src/Main.java
+++ java_web_33/src/Main.java
@@ -1,20 +1,17 @@
-
+import java.util.ArrayList;
+import java.util.function.Consumer;
+interface StringFunction {
+    String run(String str);
+}
 class Main extends Thread {
-    public static int amount = 0;
     public static void main(String[] args) {
-        Main thread = new Main();
-        thread.start();
-        // Wait for the thread to finish
-        // use isAlive() to prevent concurrency problems
-        while (thread.isAlive()) {
-            System.out.println("Waiting process...");
-        }
-        // Update amount and print its value
-        System.out.println("Main: " + amount);
-        amount++;
-        System.out.println("Main: " + amount);
+        StringFunction exclaim = (s) -> s + "!";
+        StringFunction ask = (s) -> s + "?";
+        printFormatted("Hello", exclaim);
+        printFormatted("Hello", ask);
     }
-    public void run() {
-        amount++;
+    public static void printFormatted(String str, StringFunction format) {
+        String result = format.run(str);
+        System.out.println(result);
     }
 }
