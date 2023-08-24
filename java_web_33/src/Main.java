6b26da6c0fa1f3ceaa4c8332c9452764fac5581f
TuDT <tudt@codluck.com>
2023-08-24 10:05:04 +0700

    - [Java Web NIIT-ICT 33]: Lession Generics Promotes Code Reusability

--- java_web_33/src/Main.java
+++ java_web_33/src/Main.java
@@ -2,16 +2,41 @@
 // Generic functions
 class Main extends Thread {
     // A generic method example
-    static <T> void genericDisplay(T element)
-    {
-        System.out.println(element.getClass().getName() + " = " + element);
+    static <T extends Comparable<T>> void sortGenerics(T[] a) {
+        // As we are comparing the non-primitive data types
+        // We need to use Comparable class
+        // Bubble sort logic
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j].compareTo(a[j+1]) > 0) {
+                    swap(j, j + 1, a);
+                }
+            }
+        }
+        // Printing the elements after sorted
+        for (T i : a)
+        {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
     }
+
+    public static <T> void swap(int i, int j, T[] a)
+    {
+        T t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
     public static void main(String[] args) {
-        // Calling generic method with Integer argument
-        genericDisplay(11);
-        // Calling generic method with String argument
-        genericDisplay("GeeksForGeeks");
-        // Calling generic method with double argument
-        genericDisplay(1.0);
+        Integer[] a = {100, 22, 58, 41, 6, 50};
+        Character[] c = {'v', 'g', 'a', 'c', 'x', 'd', 't'};
+        String[] s = {"Virat", "Rohit", "Abhinay", "Chandu", "Sam", "Bharat", "Kalam"};
+        System.out.println("Sorted Integer array: ");
+        sortGenerics(a);
+        System.out.println("Sorted Character array: ");
+        sortGenerics(c);
+        System.out.println("Sorted String array: ");
+        sortGenerics(s);
     }
 }
