68a6d12c138344ec2a1d50c9254dfa39f412fd09
TuDT <tudt@codluck.com>
2023-08-25 13:13:14 +0700

    - [Java Web NIIT-ICT 33]: Lession HTTP GET Request with HttpURLConnection

--- java_web_33/src/Main.java
+++ java_web_33/src/Main.java
@@ -1,42 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 // Java program to show working of user defined
 // Generic functions
-class Main extends Thread {
-    // A generic method example
-    static <T extends Comparable<T>> void sortGenerics(T[] a) {
-        // As we are comparing the non-primitive data types
-        // We need to use Comparable class
-        // Bubble sort logic
-        for (int i = 0; i < a.length - 1; i++) {
-            for (int j = 0; j < a.length - i - 1; j++) {
-                if (a[j].compareTo(a[j+1]) > 0) {
-                    swap(j, j + 1, a);
-                }
-            }
-        }
-        // Printing the elements after sorted
-        for (T i : a)
-        {
-            System.out.print(i + ", ");
-        }
-        System.out.println();
+class Main {
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "https://www.google.com/search?q=javaguides";
+    public static void main(String[] args) throws IOException {
+        sendHttpGETRequest();
     }
-
-    public static <T> void swap(int i, int j, T[] a)
-    {
-        T t = a[i];
-        a[i] = a[j];
-        a[j] = t;
-    }
-
-    public static void main(String[] args) {
-        Integer[] a = {100, 22, 58, 41, 6, 50};
-        Character[] c = {'v', 'g', 'a', 'c', 'x', 'd', 't'};
-        String[] s = {"Virat", "Rohit", "Abhinay", "Chandu", "Sam", "Bharat", "Kalam"};
-        System.out.println("Sorted Integer array: ");
-        sortGenerics(a);
-        System.out.println("Sorted Character array: ");
-        sortGenerics(c);
-        System.out.println("Sorted String array: ");
-        sortGenerics(s);
+    private static void sendHttpGETRequest() throws IOException {
+        URL obj = new URL(GET_URL);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) obj.openConnection();
+        httpURLConnection.setRequestMethod("GET");
+        httpURLConnection.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = httpURLConnection.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            } in.close();
+            // print result
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+        for (int i = 1; i <= 8; i++) {
+            System.out.println(httpURLConnection.getHeaderFieldKey(i) + " = " + httpURLConnection.getHeaderField(i));
+        }
     }
 }
