d02d7f81191b41929db33d931a3ff19de455080b
DAO TIEN TU <daotientu@gmail.com>
2023-09-05 23:10:49 +0700

    Revert "Draf"
    This reverts commit 177ad67c31910b6c2142accfc9dddcf053251c5d.

--- java_web_33/java_33/java_web/src/OptionInfo.java
+++ java_web_33/java_33/java_web/src/OptionInfo.java
@@ -2,23 +2,8 @@ import java.util.Date;
 public class OptionInfo {
     private Date create;
     private String addressCreate;
-    private Integer amount;
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int temp2) {
-        this.amount = amount;
-    }
 
     public OptionInfo() {
-    }
-
-    public OptionInfo(Date create, String addressCreate, Integer amount) {
-        this.create = create;
-        this.addressCreate = addressCreate;
-        this.amount = amount;
     }
 
     public Date getCreate() {
@@ -34,8 +19,7 @@ public class OptionInfo {
         return "OptionInfo{" +
                 "create=" + create +
                 ", addressCreate='" + addressCreate + '\'' +
-                ", amount=" + amount +
-                '}';
+                '}' + "\n";
     }
 
     public String getAddressCreate() {
