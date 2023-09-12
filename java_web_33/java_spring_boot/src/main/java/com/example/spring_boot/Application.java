111fd2e8315b49a2c36b0ace1431c08b2755b69b
TuDT <tudt@codluck.com>
2023-09-12 11:57:59 +0700

    - [Java Web NIIT-ICT 33]: Test Spring Boot Convert JSON Data to Object

--- java_web_33/java_spring_boot/src/main/java/com/example/spring_boot/Application.java
+++ java_web_33/java_spring_boot/src/main/java/com/example/spring_boot/Application.java
@@ -1,20 +1,36 @@
 package com.example.spring_boot;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class Application {
     @GetMapping(value = "/")
     public String index() {
         return "index";
     }
 
+    @GetMapping(value = "/callapiclient")
+    public String callAPIClient() throws JsonProcessingException {
+        String json = "{\"name\":\"David\",\"position\":\"SOFTWARE_ENGINEER\",\"skilltree\":[\"Java\",\"Python\",\"JavaScript\"],\"address\":{\"street\":\"Street\",\"streetNo\":\"123\"}}";
+        // ObjectMapper instantiation
+        ObjectMapper objectMapper = new ObjectMapper();
+        // Deserialization into the `Employee` class
+        Employee employee = objectMapper.readValue(json, Employee.class);
+        // Print information
+        System.out.println("Name: " + employee.getName());
+        System.out.println("Position: " + employee.getPosition());
+        System.out.println("Skilltree: " + employee.getSkilltree());
+        System.out.println("Address: " + employee.getAddress().getStreet() + ' ' + employee.getAddress().getStreetNo());
+        return employee.toString();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
---
+++ java_web_33/java_spring_boot/src/main/java/com/example/spring_boot/POSITION.java
@@ -0,0 +1,5 @@
+package com.example.spring_boot;
+
+public enum POSITION {
+    MANAGER, SOFTWARE_ENGINEER, CEO
+}
