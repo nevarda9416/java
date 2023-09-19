1b0b0f5cb68a397862b61df9a59581c88bae3131
DAO TIEN TU <daotientu@gmail.com>
2023-09-19 20:22:55 +0700

    - [Java Web NIIT-ICT 33]: Lession 9.2

--- java_web_33/java_33/java_web/src/Main.java
+++ java_web_33/java_33/java_web/src/Main.java
@@ -3,23 +3,28 @@
 // (powered by FernFlower decompiler)
 //
 
-import java.io.File;
-import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            String folder = "C:\\dev\\java\\out";
-            File fold = new File(folder);
-            if (!fold.exists()) {
-                fold.mkdir();
-                System.out.print("Nhập tên file muốn tạo: ");
-                Scanner scannerFold = new Scanner(System.in);
-                String selectFold = scannerFold.nextLine();
-                String strFile = fold.getPath() + "\\" + selectFold;
-                File file = new File(strFile);
-                file.createNewFile();
+            String path = "C:\\dev\\java\\out\\out.txt";
+            File file = new File(path);
+
+            FileOutputStream outputStream = new FileOutputStream(file, true);//true: giữ lại nội dung cũ và ghi đè nội dung mới tiếp tục
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+            bufferedWriter.write("Nguyen Van A");
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+            bufferedWriter.close();
+            outputStream.close();
+
+            FileInputStream inputStream = new FileInputStream(file);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String str;
+            while ((str = bufferedReader.readLine()) != null) {
+                System.out.println(str);
             }
         } catch (Exception exception) {
             System.out.println("Có lỗi xảy ra: " + exception.getMessage());
