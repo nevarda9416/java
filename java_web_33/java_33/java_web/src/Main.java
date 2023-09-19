335c86db4a517b0f89325a169f7f7e3606f635cb
DAO TIEN TU <daotientu@gmail.com>
2023-09-19 20:46:41 +0700

    - [Java Web NIIT-ICT 33]: Lession 9.4

--- java_web_33/java_33/java_web/src/Main.java
+++ java_web_33/java_33/java_web/src/Main.java
@@ -3,31 +3,47 @@
 // (powered by FernFlower decompiler)
 //
 
+import Models.Customer;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            String path = "C:\\dev\\java\\out\\out.txt";
-            File file = new File(path);
-
-            FileOutputStream outputStream = new FileOutputStream(file, true);//true: giữ lại nội dung cũ và ghi đè nội dung mới tiếp tục
-            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
-            bufferedWriter.write("Nguyen Van A");
-            bufferedWriter.newLine();
-            bufferedWriter.flush();
-            bufferedWriter.close();
-            outputStream.close();
-
-            FileInputStream inputStream = new FileInputStream(file);
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String str;
-            while ((str = bufferedReader.readLine()) != null) {
-                System.out.println(str);
-            }
+            Main main = new Main();
+            main.getAll();
         } catch (Exception exception) {
             System.out.println("Có lỗi xảy ra: " + exception.getMessage());
         }
+    }
+
+    public List<Customer> getAll() throws IOException {
+        String path = "C:\\dev\\java\\out\\out.txt";
+        List<Customer> customers = readFile(path);
+        return customers;
+    }
+
+    public List<Customer> readFile(String file) throws IOException {
+        List<Customer> customers = new ArrayList<>();
+        File f = new File(file);
+        FileInputStream inputStream = new FileInputStream(file);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String str;
+        while ((str = bufferedReader.readLine()) != null) {
+            String[] data = str.split("   ");
+            Customer customer = new Customer();
+            customer.setId(Integer.parseInt(data[0]));
+            customer.setName(data[1]);
+            customer.setAge(Integer.parseInt(data[2]));
+            customer.setAddress(data[3]);
+            customers.add(customer);
+        }
+        System.out.println(customers);
+        inputStream.close();
+        bufferedReader.close();
+        return customers;
     }
 }
