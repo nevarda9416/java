2b43176229da671bd50b8904ad6f0f49f55d1d73
DAO TIEN TU <daotientu@gmail.com>
2023-10-05 20:50:30 +0700

    - [Java Web NIIT-ICT 33]: Lession 10.5 DELETE DATA

--- java_web_33/java_33/demo_connect_sql/src/Main.java
+++ java_web_33/java_33/demo_connect_sql/src/Main.java
@@ -1,16 +1,35 @@
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-// Thực thi 1 transaction đơn giản sau
+// Thực thi các transaction đơn giản sau
 public class Main {
     public static void main(String[] args) {
+        Main main = new Main();
+        main.findByName();
+        main.deleteById();
+    }
+
+    public Connection openConnection() {
         String DB_URL = "jdbc:mysql://localhost:3306/java_web_33?useSSL=false";
         String USER_NAME = "root";
         String PASSWORD = "admin";
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            return connection;
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+            return null;
+        }
+    }
+
+    public List<Product> findByName() {
+        List<Product> productList = new ArrayList<>();
+        Connection connection = openConnection();
+        try {
             String sql = "SELECT * FROM products WHERE name LIKE ?";
             PreparedStatement statement = connection.prepareStatement(sql);
             Scanner scanner = new Scanner(System.in);
@@ -19,10 +38,36 @@ public class Main {
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 System.out.println("Sản phẩm có tồn tại");
+                Product product = new Product();
+                product.setId(resultSet.getInt("id"));
+                product.setName(resultSet.getString("name"));
+                product.setMade_in(resultSet.getString("made_in"));
+                product.setStatus(resultSet.getInt("status"));
+                productList.add(product);
+                System.out.println(product.toString());
             } else {
                 System.out.println("Sản phẩm không tồn tại");
             }
-            connection.close();
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+        return productList;
+    }
+
+    public void deleteById() {
+        Connection connection = openConnection();
+        try {
+            String sql = "DELETE FROM products WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Nhập ID sản phẩm cần xóa: ");
+            statement.setInt(1, scanner.nextInt());
+            int result = statement.executeUpdate();
+            if (result == 1) {
+                System.out.println("Xóa sản phẩm thành công");
+            } else {
+                System.out.println("ID không tồn tại. Xóa sản phẩm thất bại");
+            }
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
